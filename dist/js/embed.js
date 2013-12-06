a24d8406631350d8661e7fc000c2ca985eab4b6e
Andrew Mason <andrew.mason@guardian.co.uk>
2013-12-06 12:42:08 +0000

    Updated embed page

--- dist/js/embed.js
+++ dist/js/embed.js
@@ -1,8 +1,7 @@
 var prodDataURL = 'http://interactive.guim.co.uk/spreadsheetdata/0AkRR3zKqdlUHdDI1NzZ2RVJSdGNOek9WWTdiUUxyTEE.jsonp';
-var testDataURL = 'http://interactive.guim.co.uk/spreadsheetdata/0AjNAJ9Njg5YTdEx0SFh4cFh1MmtveUR5YlZxbEpjZ2c.jsonp';
 var $output = $('#output');
 var $entryList = $('#entryList');
-var embedURL = 'http://s3-eu-west-1.amazonaws.com/gdn-stage/antarctica-2013/index.html?update_id=';
+var embedURL = 'http://interactive.guim.co.uk/embed/antarctica-2013/';
 var iframeCode = '<iframe src="{{ src }}" style="width: 100%; height: 560px; overflow: hidden;" seamless frameborder="0" scrolling="no"></iframe>';
 var entries;
 var selectedEntry;
@@ -11,7 +10,7 @@ function getData() {
     $.ajax({
         type:'get',
         dataType:'jsonp',
-        url: testDataURL,
+        url: prodDataURL,
         jsonpCallback: 'gsS3Callback',
         cache: true
     });
@@ -27,7 +26,13 @@ function listEntries() {
     entries.forEach(function(entry, index) {
         var item = $('<li></li>').html(entry.date);
         item.on('click', function() {
-            selectEntry(index);
+            if ($(this).hasClass('active')) {
+                selectEntry(null);
+                $(this).removeClass('active');
+                return;
+            }
+
+            selectEntry(parseInt(index, 10));
             $entryList.find('li').removeClass('active');
             $(this).addClass('active');
         });
@@ -36,19 +41,27 @@ function listEntries() {
 }
 
 function selectEntry(index) {
-    var selectedIndex = ('undefined' === typeof index) ? entries.length-1 : index ;
-    selectedEntry = selectedIndex;
+    selectedEntry = index;
     updatePreview();
     updateEmbedCode();
 }
 
 function updatePreview() {
-    var iframeSrc = embedURL + entries[selectedEntry].updatenumber;
+    var iframeSrc = embedURL;
+    if (selectedEntry !== null && typeof selectedEntry === 'number') {
+        console.log(selectedEntry);
+        iframeSrc += '?update_id=' + entries[selectedEntry].updatenumber;
+    }
     $('iframe').attr('src', iframeSrc);
 }
 
 function updateEmbedCode() {
-    var embedCode = iframeCode.replace('{{ src }}', embedURL + entries[selectedEntry].updatenumber);
+    var iframeSrc = embedURL;
+    if (selectedEntry !== null && typeof selectedEntry === 'number') {
+        console.log(selectedEntry);
+        iframeSrc += '?update_id=' + entries[selectedEntry].updatenumber;
+    }
+    var embedCode = iframeCode.replace('{{ src }}', iframeSrc);
     $output.val(embedCode);
 }
 
